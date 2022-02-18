cc09547391b64d82763918491ec1b3f3191aa226
grayTurtle01 <the.correo@protonmail.com>
2022-02-18 13:47:58 -0600

    challenge-02: coords_2_index( coords)

--- 02_knight-moves.py
+++ 02_knight-moves.py
@@ -1,6 +1,15 @@
 def index_2_coords(index):
-    x = (index % 8) + 1
     y = (index // 8) + 1
+    x = (index % 8) + 1
 
     return (y, x)
+
+def coords_2_index( coords ):
+    y, x = coords 
+
+    index = (y-1)*8 + (x-1)
+    return index
+
+    
+
 
